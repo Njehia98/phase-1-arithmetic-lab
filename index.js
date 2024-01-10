bc65a8d5e9f98a5ca4c966f5d9f5f67abb9a15a8
Njehia98 <stacynjihia028@gmail.com>
2024-01-10 22:55:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+function multiply(num1, num2) {
+  var result = num1 * num2;
+  return result;
+}
+var num1 = 31;
+var num2 = 2;
+var result = multiply(num1, num2);
+console.log(result); // Output: 62
 
-// Write your code here
+var random = Math.floor(Math.random() * 14); // Change 100 to your desired range
+console.log(random);
+
+function mod(num3,num4) {
+    var result=num3/num4;
+    return result;
+}
+var num3=12;
+var num4=8;
+var mod=num3%num4;
+console.log(mod); // output 4
+    var num1 = 10;
+    var num2 = 15;
+    var num3 = 20;
+    var num4 = 5;
+    
+    // Find the highest number and assign it to the variable max
+    var max = Math.max(num1, num2, num3, num4);
+    console.log(max); // Output: 20
+    
